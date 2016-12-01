4bbb6f1bd75f2fbffbe3660d9ff8032ce196ce7a
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-12-01 17:45:25 +0100

    task no.1 complete

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+def value_doubler(input_list):
+    new_list = []
+    if type(input_list) == list:
+        new_list = [i * 2 for i in input_list]
+        return new_list
+    else:
+        raise TypeError
